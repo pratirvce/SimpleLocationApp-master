459d6b9d0ddac73bf29fdc4442403fe8a22efb37
pratirvce <pratirvce@gmail.com>
2016-10-12 19:51:40 -0700

    Initial commit

---
+++ app/src/main/java/com/sample/foo/simplelocationapp/Config.java
@@ -0,0 +1,11 @@
+package com.sample.foo.simplelocationapp;
+
+/**
+ * Created by prati on 10/12/2016.
+ */
+
+public class Config {
+    public static final String FIREBASE_URL = "https://simplelocationapp-fc341.firebaseio.com/";
+
+
+}
